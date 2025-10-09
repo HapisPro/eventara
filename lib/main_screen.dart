7c681f6b3820181d3e010f61e5b29fcd0962df9a
M. Hafizh Al Kautsar <96516537+alkausarhapis@users.noreply.github.com>
2025-10-09 14:36:31 +0700

    Feature add event (#3)
    * init project
    
    * Mempercantik detail page
    
    * Make home : detail page
    
    * init assets
    
    * add image
    
    * add image
    
    * add event
    
    ---------
    
    Co-authored-by: miftahasiboean <miftaheczson027@gmail.com>

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eventara/screen/add_event_screen.dart';
 import 'package:eventara/test_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,7 +19,7 @@ class _MainScreenState extends State<MainScreen> {
   final List<Widget> _pages = [
     const HomeScreen(),
     const TestScreen(),
-    const TestScreen(),
+    const AddEventScreen(),
     const TestScreen(),
     const TestScreen(),
   ];
